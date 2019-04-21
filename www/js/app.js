da99b7f255b11082cd968975321b73241e07fd29
Haseko <valerii777999@gmail.com>
2019-04-21 18:01:27 +0900

    ASDF

--- www/js/app.js
+++ www/js/app.js
@@ -13,192 +13,24 @@ var app = new Framework7({
 
         return {
             url: 'http://9142767578.myjino.ru',
+            price: 0,
             user: {
                 firstName: 'John',
                 lastName: 'Doe',
             },
-            // My Cart of products
+
             cart: [
-
             ],
-            // Demo products for Catalog section
-            category: {},
 
-
-                /*
-                Burgers: {
-                    id: '1',
-                    title: 'Burgers',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.',
-                    category: 'Burgers',
-                    recipes: [
-                        1, 2, 3
-                    ]
-                },
-                Potato: {
-                    id: '2',
-                    title: 'Potato with cheese',
-                    description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!',
-                    category: 'Potato',
-                    recipes: [
-                        1, 2, 3
-                    ]
-                },
-                Lasagna: {
-                    id: '3',
-                    title: 'Lasagna',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Lasagna',
-                    recipes: [
-                        1, 2, 3
-                    ]
-                },
-                Taco: {
-                    id: '4',
-                    title: 'Taco',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Taco',
-                    recipes: [
-                        1, 2, 3
-                    ]
-                },
-                Burgers: {
-                    id: '5',
-                    title: 'Nachos',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Burgers',
-                    recipes: [
-                        1, 2, 3
-                    ]
-                },
-                Burgers: {
-                    id: '6',
-                    title: 'Taquitos',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Burgers',
-                    recipes: [
-                        1, 2, 3
-                    ]
-                },
-                */
+            category: {
+            },
 
             recipeList: {
-                1: {
-                    name: 'a',
-                    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    products: [1, 2, 3],
-                    description: 'v',
-                    img: '1',
-                    id: '1'
-                },
-                2: {
-                    name: 'b',
-                    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    products: [1, 2, 3],
-                    description: 'v',
-                    img: '1',
-                    id: '1'
-                },
-
-                3: {
-                    name: 'c',
-                    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    products: [1, 2, 3],
-                    description: 'v',
-                    img: '1',
-                    id: '1'
-                }
             },
 
             productList: {
-                1: [{
-
-                        name: '1',
-                        img: '1',
-                        description: '1',
-                        markets: [1, 2, 3]
-
-                    },
-                    {
-
-                        name: '1',
-                        img: '1',
-                        description: '1',
-                        markets: [1, 2, 3]
-
-                    }
-                ],
-                2: [{
-
-                        name: '1',
-                        img: '1',
-                        description: '1',
-                        markets: [1, 2, 3]
-
-                    },
-                    {
-
-                        name: '1',
-                        img: '1',
-                        description: '1',
-                        markets: [1, 2, 3]
-
-                    }
-                ],
-                3: [{
-
-                        name: '1',
-                        img: '1',
-                        description: '1',
-                        markets: [1, 2, 3]
-
-                    },
-                    {
-
-                        name: '1',
-                        img: '1',
-                        description: '1',
-                        markets: [1, 2, 3]
-
-                    }
-                ]
             },
-            products: [{
-                    id: '1',
-                    title: 'Burgers',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.',
-                    category: 'Burgers'
-                },
-                {
-                    id: '2',
-                    title: 'Potato with cheese',
-                    description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!',
-                    category: 'Potato'
-                },
-                {
-                    id: '3',
-                    title: 'Lasagna',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Lasagna'
-                },
-                {
-                    id: '4',
-                    title: 'Taco',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Taco'
-                },
-                {
-                    id: '5',
-                    title: 'Nachos',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Nachos'
-                },
-                {
-                    id: '6',
-                    title: 'Taquitos',
-                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.',
-                    category: 'Taquitos'
-                }
+            products: [
             ]
         };
     },
@@ -208,17 +40,22 @@ var app = new Framework7({
 
             $$('#view-catalog .cartOutList').html('');
 
+            app.data.price = 0;
 
             app.data.cart.forEach(function(v, i) {
 
                 $$('#view-catalog .cartOutList').append('<li class="swipeout"><div class="item-content swipeout-content"><div class="item-inner"><div class="item-title">'
-                  + app.data.productList[v].name+
-                  '</div></div></div>'+
+                  + app.data.productList[v-1].name+
+                  '</div><span class="float-right">' + app.data.productList[v-1].price +'</span></div></div>'+
                   '<div class="swipeout-actions-right">'+
-                  '<a href="#" class="swipeout-delete">Delete</a>'+
+                  '<a href="#" class="swipeout-delete" onclick = "app.data.cart.splice('+i+', 1); app.data.price -= '+app.data.productList[v-1].price+'; $$(\'#price\').html(app.data.price);">Delete</a>'+
                   '</div></li>');
+                  app.data.price += parseInt(app.data.productList[v-1].price);
 
             });
+
+            $$('#price').html(app.data.price);
+
 
         },
     },
@@ -287,4 +124,10 @@ $$('#my-login-screen .login-button').on('click', function() {
 
     // Alert username and password
     app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
+
+
+$$('.deleted-callback').on('swipeout:deleted', function () {
+    console.log($$(this).prop('myid'));
+    alert(1);
 });
